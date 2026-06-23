34149e76e0a95e223e2b937d31300515fe31f8f1
pixl <216277949+un-pixelated@users.noreply.github.com>
2026-06-23 09:16:54 +0530

    [calc] Add nature input, stub out optimise()

--- calc.py
+++ calc.py
@@ -15,10 +15,23 @@ def damage_formula(P: int, A: int, D: int, M: list[float]) -> int:
 
 BASE_HP = int(input("BASE HP:  "))
 BASE_DEF = int(input("BASE DEF: "))
+NATURE = float(input("NATURE:   "))
 BUDGET = int(input("BUDGET:   "))
 
 POWER = int(input("POWER:    "))
 ATTACK = int(input("ATTACK:   "))
 MULT = list(input("MULT:     "))
+
+def optimise(BASE_HP: int,
+             BASE_DEF: int,
+             NATURE: float,
+             BUDGET: int,
+             POWER: int,
+             ATTACK: int,
+             MULT: list[float]
+             ) -> float:
+    HP = BASE_HP + 75
+    
+
 
 print(damage_formula(75, 65, 123, 163, [0.85, 1.5, 4]))
